d23ec26f5d553200d5e0ebf52bdcc24b46884766
lennypeers <lenny.peers@gmail.com>
2021-08-16 14:46:07 +0200

    mmf v0.2

--- PKGBUILD
+++ PKGBUILD
@@ -1,6 +1,6 @@
 # Maintainer: lennypeers <lennypeers+git at protonmail>
 pkgname=mmf
-pkgver=0.1
+pkgver=0.2
 pkgrel=1
 pkgdesc="manual mac fan"
 arch=(x86_64)
--- src/mmf.sh
+++ src/mmf.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 hwmon=/sys/devices/platform/applesmc.768
+readonly VERSION=0.2
 
 usage() {
   cat << EOF
@@ -9,18 +10,41 @@ Fan speed control utility
 Available commands:
 SPEED                  set the fan at SPEED rpm
 -t, --toggle, toggle   toggle manual speed
+-v, --version          display misc infos
 -h, --help             show this message
+EOF
+}
+
+infos() {
+  cat << EOF
+mmf v${VERSION}
+MIT License
+Copyright (c) 2021 lennypeers
 EOF
 }
 
 toggle() {
   local tmp
 
-  [[ $(< ${hwmon}/fan1_manual) = 1 ]] && tmp=0
+  if [[ $(cat ${hwmon}/fan1_manual) = 1 ]]; then
+    tmp=0
+  fi
+
   echo ${tmp:=1} | tee ${hwmon}/fan1_manual
 
-  [[ -f ${hwmon}/fan2_manual ]] && \
-  echo ${tmp} | tee ${hwmon}/fan2_manual
+  if [[ -e ${hwmon}/fan2_manual ]]; then
+    echo ${tmp:=1} | tee ${hwmon}/fan2_manual
+  fi
+}
+
+write() {
+    echo 1 > ${hwmon}/fan1_manual
+    echo ${1/k/000} > ${hwmon}/fan1_output
+
+    if [[ -e ${hwmon}/fan2_output ]]; then
+      echo 1 > ${hwmon}/fan2_manual
+      echo ${1/k/000} > ${hwmon}/fan2_output
+    fi
 }
 
 case $1 in
@@ -32,6 +56,11 @@ case $1 in
     usage
   ;;
 
+  -v | --version)
+    infos
+    exit 0
+  ;;
+
   "")
     usage >&2
     exit 1
@@ -39,11 +68,7 @@ case $1 in
 
   # hum assuming it is a digit lol
   *)
-    [[ $(< ${hwmon}/fan1_manual) != 1 ]] && echo 1 > ${hwmon}/fan1_manual
-    echo ${1/k/000} > ${hwmon}/fan1_output
-
-    [[ -f ${hwmon}/fan2_output ]] && \
-    echo ${1/k/000} > ${hwmon}/fan2_output
+    write "${1/k/000}"
   ;;
 esac
 
